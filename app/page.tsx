f1a6e8b4b8ae7759108adf73b5782df2f5e57c47
HuyDNA <huydo862003@gmail.com>
2024-03-24 22:02:25 +0700

    feat: add Toaster

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-      </div>
-    </main>
+    <div>
+    </div>
   );
 }
