af139ae46b8a66323a5f0216a0e2501d9c1875b1
HuyDNA <huydo862003@gmail.com>
2024-04-05 06:55:21 +0700

    fix: patch old class

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
       <head />
       <body
         className={cn(
-          "min-h-screen bg-background font-sans antialiased",
+          "min-h-screen bg-neutral-very-light font-sans antialiased m-12",
           fontSans.variable
         )}
       >
