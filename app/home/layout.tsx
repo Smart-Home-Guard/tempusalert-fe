eace55c50bdb30169a6014b458a700af4e68ec6e
HuyDNA <huydo862003@gmail.com>
2024-04-16 07:26:18 +0700

    fix: homepage layout redirect import

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -1,8 +1,18 @@
+"use client"
+
 import "@/styles/globals.css";
 
 import { NavigationBar } from "./navbar";
+import { useLocalStorage } from "usehooks-ts";
+import { redirect } from "next/navigation";
 
 export default function HomeLayout({ children }: RootLayoutProps) {
+  const [loggedIn] = useLocalStorage("loggedIn", false);
+
+  if (!loggedIn) {
+    redirect('/login');
+  }
+
   return (
       <div className="flex min-h-screen bg-neutral-light">
         <NavigationBar />
