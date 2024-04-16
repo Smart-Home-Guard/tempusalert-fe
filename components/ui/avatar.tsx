71ee111cec68e7ef1fe11457b8931d8b4fda0b52
HuyDNA <huydo862003@gmail.com>
2024-04-16 07:43:41 +0700

    fix: avatar size

--- components/ui/avatar.tsx
+++ components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<
   <AvatarPrimitive.Root
     ref={ref}
     className={cn(
-      "relative flex h-12 w-12 shrink-0 overflow-hidden rounded-full",
+      "relative flex h-32 w-32 shrink-0 overflow-hidden rounded-full",
       className
     )}
     {...props}
