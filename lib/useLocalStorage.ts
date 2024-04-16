d8154f3f69f8bb0536dcb88d01dd854a28559531
HuyDNA <huydo862003@gmail.com>
2024-04-16 13:35:25 +0700

    fix: typing for useLocalStorage

--- lib/useLocalStorage.ts
+++ lib/useLocalStorage.ts
@@ -1,8 +1,8 @@
-import { useEffect, useState } from "react";
+import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { useLocalStorage as _useLocalStorage } from "usehooks-ts";
 
 // Unlike useLocalStorage from usehooks-ts, this version does not cause hydration mismatch
-export function useLocalStorage<T>(key: string, defaultValue: T) {
+export function useLocalStorage<T>(key: string, defaultValue: T): [T, Dispatch<SetStateAction<T>>, () => void] {
     const [keyState, setKeyState] = useState(defaultValue);
     const [keyLocalStorage, setKeyLocalStorage, removeKeyLocalStorage] = _useLocalStorage(key, defaultValue);
     useEffect(() => {
