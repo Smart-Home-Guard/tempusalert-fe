71e77f58e29190840caa38cc9fe0de9daeecc1b6
quannhg <quan.nguyenhg12@gmail.com>
2024-05-07 23:03:35 +0700

    fix: remove redundant quote

--- lib/apiClient.ts
+++ lib/apiClient.ts
@@ -3,13 +3,13 @@ import createClient, { HeadersOptions } from "openapi-fetch";
 
 function getHeaders(): HeadersOptions {
   if (typeof window !== "undefined") {
-    return { "jwt": localStorage.getItem("jwt") || '' };
+    return { jwt: localStorage.getItem("jwt")?.slice(1, -1) || "" };
   }
   return {};
 }
 
 export const apiClient = createClient<paths>({
   baseUrl: process.env.NEXT_PUBLIC_BACKEND_URL,
-  credentials: 'include',
+  credentials: "include",
   headers: getHeaders(),
 });
