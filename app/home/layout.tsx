76a81d5405437664890a867962f1f15ca1ea5f29
Huy-DNA <huydo862003@gmail.com>
2024-05-10 14:36:41 +0700

    fix: refactor app breadcrumb

--- app/home/layout.tsx
+++ app/home/layout.tsx
@@ -3,11 +3,11 @@
 import "@/styles/globals.css";
 
 import { NavigationBar } from "./navbar";
-import { redirect } from "next/navigation";
-import { useEffect, useTransition } from "react";
+import { redirect, usePathname } from "next/navigation";
+import { Fragment, useEffect, useTransition } from "react";
 import { useLoggedInStore } from "@/store";
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList } from "@/components/ui/breadcrumb";
-import { HomeIcon } from "lucide-react";
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, BreadcrumbList, BreadcrumbSeparator } from "@/components/ui/breadcrumb";
+import { FlameIcon, HomeIcon, Slash } from "lucide-react";
 
 export default function HomeLayout({
   children,
@@ -27,16 +27,53 @@ export default function HomeLayout({
     <div className="flex flex-row min-h-screen gap-4 bg-neutral-very-light">
       <NavigationBar />
       <div className="p-12 w-full transform transition duration-300 ease-in-out">
-        <Breadcrumb className="mb-8">
-          <BreadcrumbList>
-            <BreadcrumbItem className="flex items-center">
-              <HomeIcon size={18} />
-              <BreadcrumbLink href="/">Home</BreadcrumbLink>
-            </BreadcrumbItem>
-          </BreadcrumbList>
-        </Breadcrumb>
+        <AppBreadcrumb />
         {children}
       </div>
     </div>
   );
+}
+
+function AppBreadcrumb() {
+  const pathname = usePathname();
+  const pathComponents = pathname.split("/").slice(1);
+
+  const itemMap: { [index: string]: Record<"href", string> & Record<"icon", typeof HomeIcon> & Record<"name", string> } = {
+    home: {
+      icon: HomeIcon,
+      href: "/home/",
+      name: "Home",
+    },
+    "fire-alert": {
+      icon: FlameIcon,
+      href: "/home/fire-alert",
+      name: "Fire alert",
+    },
+  };
+
+  return (
+    <Breadcrumb className="mb-8">
+      <BreadcrumbList>
+        {
+          pathComponents.map((p) => {
+            const Icon = itemMap[p].icon;
+            const href = itemMap[p].href;
+            const name = itemMap[p].name;
+
+            return (
+              <Fragment key={p}>
+                <BreadcrumbItem className="flex items-center">
+                  <Icon size={18} />
+                  <BreadcrumbLink href={href}>{name}</BreadcrumbLink>
+                </BreadcrumbItem>
+                <BreadcrumbSeparator>
+                  <Slash />
+                </BreadcrumbSeparator>
+              </Fragment>
+            )
+          })
+        }
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
 }
